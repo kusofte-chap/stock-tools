c24e0b8c99f85ecc72f40b15b76a31ce275401b7
王石川 <gavin@192.168.1.6>
2023-09-28 11:17:22 +0800

    update

--- src/page/Layout.jsx
+++ src/page/Layout.jsx
@@ -1,19 +1,20 @@
 // import React from 'react'
 import {
   Button,
-  Collapse,
   Stack,
   List,
   ListItemButton,
   ListItemIcon,
   ListItemText,
   Divider,
+  IconButton,
 } from "@mui/material";
 import { Link, Outlet, useNavigate } from "react-router-dom";
 
 import AssessmentIcon from "@mui/icons-material/Assessment";
 // import TableViewIcon from "@mui/icons-material/TableView";
 import WestIcon from "@mui/icons-material/West";
+import EastIcon from "@mui/icons-material/East";
 // import ShowChartIcon from "@mui/icons-material/ShowChart";
 import CandlestickChartIcon from "@mui/icons-material/CandlestickChart";
 import { useState } from "react";
@@ -25,85 +26,71 @@ export default function Layout() {
 
   return (
     <Stack width="100vw" height="100vh" flexDirection="row" overflow="hidden">
-      <Collapse in={open}>
-        <Stack
-          sx={{
-            width: open ? 240 : "50px",
-            height: "100vh",
+      <Stack
+        sx={{
+          width: open ? 240 : "56px",
+          height: "100vh",
+          flexShrink: 0,
+          borderRight: "1px solid #ccc",
+          justifyContent: "space-between",
+          transition: "width 0.2s ease",
+        }}
+      >
+        <Link
+          to="/"
+          style={{
+            width: "100%",
+            height: "38px",
+            textAlign: "center",
+            lineHeight: "38px",
             flexShrink: 0,
-            borderRight: "1px solid #ccc",
-            justifyContent: "space-between",
-            transition: "width 0.2s ease",
+            textDecoration: "none",
+            ontWeight: 500,
+            color: "#000",
+            fontSize: open ? 18 : 15,
+            textTransform: "uppercase",
+            borderBottom: "1px solid #E5E5E5",
           }}
         >
-          <Link
-            to="/"
-            style={{
-              width: "100%",
-              height: "38px",
-              textAlign: "center",
-              lineHeight: "38px",
-              flexShrink: 0,
-              textDecoration: "none",
-              ontWeight: 500,
-              color: "#000",
-              fontSize: 18,
-              textTransform: "uppercase",
-              borderBottom: "1px solid #E5E5E5",
-            }}
-          >
-            STOCK TOOLs
-          </Link>
-          <Stack flex={1}>
-            <List
-              sx={{
-                width: "100%",
-                maxWidth: 360,
-                bgcolor: "background.paper",
-              }}
-              component="nav"
-              aria-labelledby="nested-list-subheader"
-            >
-              <ListItemButton sx={{ pl: 2 }} onClick={() => ng("/chart")}>
-                <ListItemIcon>
-                  <CandlestickChartIcon />
-                </ListItemIcon>
-                <ListItemText primary="Price Tend" />
-              </ListItemButton>
-              <Divider />
-              <ListItemButton sx={{ pl: 2 }} onClick={() => ng("/FR")}>
-                <ListItemIcon>
-                  <AssessmentIcon />
-                </ListItemIcon>
-                <ListItemText primary="Fiancial Reports" />
-              </ListItemButton>
-              <Divider />
-              {/* <ListItemButton sx={{ pl: 2 }} onClick={() => ng("/FR")}>
-                <ListItemIcon>
-                  <TableViewIcon />
-                </ListItemIcon>
-                <ListItemText primary="Risk" />
-              </ListItemButton>
-              <Divider />
-              <ListItemButton sx={{ pl: 2 }} onClick={() => ng("/FR")}>
-                <ListItemIcon>
-                  <ShowChartIcon />
-                </ListItemIcon>
-                <ListItemText primary="TEND" />
-              </ListItemButton>
-              <Divider /> */}
-            </List>
-          </Stack>
-          <Stack
+          {open ? "  STOCK TOOLs" : "T"}
+        </Link>
+        <Stack flex={1}>
+          <List
             sx={{
-              alignItems: "center",
-              justifyContent: "center",
-              borderTop: "1px solid #E5E5E5",
-              height: "50px",
-              pt: 2,
-              pb: 4,
+              width: "100%",
+              maxWidth: 360,
+              bgcolor: "background.paper",
             }}
+            component="nav"
+            aria-labelledby="nested-list-subheader"
           >
+            <ListItemButton sx={{ pl: 2 }} onClick={() => ng("/chart")}>
+              <ListItemIcon>
+                <CandlestickChartIcon />
+              </ListItemIcon>
+              {open && <ListItemText primary="Price Tend" />}
+            </ListItemButton>
+            <Divider />
+            <ListItemButton sx={{ pl: 2 }} onClick={() => ng("/FR")}>
+              <ListItemIcon>
+                <AssessmentIcon />
+              </ListItemIcon>
+              {open && <ListItemText primary="Fiancial Reports" />}
+            </ListItemButton>
+            <Divider />
+          </List>
+        </Stack>
+        <Stack
+          sx={{
+            alignItems: "center",
+            justifyContent: "center",
+            borderTop: "1px solid #E5E5E5",
+            height: "50px",
+            pt: 2,
+            pb: 4,
+          }}
+        >
+          {open ? (
             <Button
               endIcon={<WestIcon />}
               onClick={() => toggleOpen((old) => !old)}
@@ -116,9 +103,14 @@ export default function Layout() {
             >
               收起
             </Button>
-          </Stack>
+          ) : (
+            <IconButton color="primary" onClick={() => toggleOpen(true)}>
+              <EastIcon />
+            </IconButton>
+          )}
         </Stack>
-      </Collapse>
+      </Stack>
+
       <Stack
         sx={{
           flex: 1,
